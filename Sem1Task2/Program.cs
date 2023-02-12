0f16de334be0ef59f1641637c58a7ace7c6228f4
inga-balode <ingaa.balode@gmail.com>
2023-02-12 22:06:52 +0200

    Seminar 1 task solution

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число: ");
+//Считываем данные с консоли
+string? x = Console.ReadLine();
+
+Console.WriteLine("C чем хотите сравнить: ");
+string? y = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (x != null && y != null)
+{
+    //Парсим введенные числа
+    int num1 = int.Parse(x);
+    int num2 = int.Parse(y);
+
+    //Сравниваем
+    if (num1 > num2)
+    {
+        Console.WriteLine("Число "+num1+" больше чем "+num2);
+    }
+    if (num1 < num2)
+    {
+        Console.WriteLine("Число "+num2+" больше чем "+num1);
+    }
+    else
+    {
+        Console.WriteLine("Числа равны");
+    }
+}
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7 
+
+
+Console.WriteLine("Введите число: ");
+//Считываем данные с консоли
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Еще одно: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Еще давай: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[] {x, y, z};
+
+//Проверим что все записалось в массив
+//var str = string.Join(" ", arr);
+//Console.WriteLine(str);
+
+Console.WriteLine("Максимальное число: " + arr.Max());
